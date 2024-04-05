bdbb955bdbf94c64692ac67060232f606c93d0b8
LanDev01 <alnjr0001@outlook.com>
2024-04-05 10:58:27 -0300

    feat: initializing prism and fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello nlw!")
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return "Hello Nlw";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP Server Running!");
+});
